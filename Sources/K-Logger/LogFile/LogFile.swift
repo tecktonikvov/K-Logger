7834c26c0c027181d245f64e5a4f4757829e6e75
Volodymyr Kotsiubenko <kotsubenkovova@gmail.com>
2024-04-29 14:43:30 +0300

    Fixed content property visibility

--- Sources/K-Logger/LogFile/LogFile.swift
+++ Sources/K-Logger/LogFile/LogFile.swift
@@ -9,7 +9,7 @@ import Foundation
 public struct LogFile {
     let fileUrl: URL
     
-    var content: String {
+    public var content: String {
         let text = try? String(contentsOf: fileUrl, encoding: .utf8)
         return text ?? ""
     }
